02337c4efa86daf5275378cdfb5b17cc987a0811
Fonsdoggen <70898032+Fonsdoggen@users.noreply.github.com>
2021-10-08 12:39:18 +0200

    links toegevoegd in de nav
    een aantal links toegevoegd

--- views/header.php
+++ views/header.php
@@ -1,3 +1,8 @@
 <header>
-
+    <nav>
+        <a href="../queen/rhapsody.php">Bohemian Rhapsody</a> |
+        <a href="../qeeun/killer.php">Killer Qeeun</a> |
+        <a href="../muse/madness.php">Madness</a> |
+        <a href=""></a>
+    </nav>
 </header>
