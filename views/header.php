b2b6300451415a70806af2c04a5e3da0ff2a9678
WoodierCupd <74969069+WoodierCupd@users.noreply.github.com>
2021-10-11 13:33:36 +0200

    edit header

--- views/header.php
+++ views/header.php
@@ -1,8 +1,21 @@
 <header>
-    <nav>
-        <span id='ct' ></span> |
-        <a href="../index.php">Home</a> |
-        <a href="songs/song.php">Songs</a> |
-        <a href="wiki/wiki.php">Informatie</a>
-    </nav>
+    <div class="dropdown">
+        <a href="http://localhost/nightshifts/"><button class="dropbtn">Home</button></a>
+    </div>
+    <div class="dropdown">
+        <button class="dropbtn">Songs</button>
+        <div class="dropdown-content">
+            <a href="#">Queen</a>
+            <a href="#">Iemand anders</a>
+            <a href="#">etc</a>
+        </div>
+    </div>
+    <div class="dropdown">
+        <button class="dropbtn">Tools</button>
+        <div class="dropdown-content">
+            <a href="#">Hammer</a>
+            <a href="#">Iets anders</a>
+            <a href="#">etc</a>
+        </div>
+    </div>
 </header>
