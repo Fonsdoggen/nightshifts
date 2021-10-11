187b2d6b0f16f9e5a4f8f67ceb0c8dfeefe368d4
Fonsdoggen <70898032+Fonsdoggen@users.noreply.github.com>
2021-10-11 13:28:10 +0200

    kleur en link
    de kleur van de bohemian rhapsody veranderd en de css link verbetered

--- songs/queen/rhapsody.php
+++ songs/queen/rhapsody.php
@@ -4,10 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?php echo $base_url; ?>css/main.css">
+    <link rel="stylesheet" href="../../css/main.css">
     <title>Songtekst Bohemian Rhapsody</title>
 </head>
 <body class="qeeun">
+    <?php require_once'../../views/header.php' ?>
     <p>
         Is this the real life?<br>
         Is this just fantasy?<br>
