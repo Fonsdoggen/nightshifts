81e9763452fdd594d685df6e98fc932e805227a8
Fonsdoggen <70898032+Fonsdoggen@users.noreply.github.com>
2021-10-11 12:44:47 +0200

    songtekst en kleur
    heb de songtekst en kleur van de pagina gefixed

--- songs/song.php
+++ songs/song.php
@@ -4,13 +4,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?php echo $base_url; ?>../css/main.css">
     <title>Songteksten en Artiesten</title>
 </head>
 <body>
     <?php require_once'../views/header.php'?>
     <h2>Qeeun</h2>
     <a href="queen/rhapsody.php">Bohemian rhapsody songtekst</a> |
-    <a href="queen/killer.php">Killer qeeun songtekst</a>
+    <a href="queen/killer.php">Killer queen songtekst</a>
     <h2>Muse</h2>
     <a href="muse/madness.php">Madness songtekst</a>
 </body>
