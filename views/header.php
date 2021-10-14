e293842aced204ed5ca0e8bb479faff560d11bda
WoodierCupd <74969069+WoodierCupd@users.noreply.github.com>
2021-10-14 10:32:55 +0200

    link in logo

--- views/header.php
+++ views/header.php
@@ -1,8 +1,7 @@
 <header onload=display_ct();>
-    <div class="dropdown">
-        <a href="http://localhost/nightshifts/"><button class="dropbtn">Home</button></a>
+    <div class="logo">
+        <a href="https://agar.io"><img src="img/codecamp.png" alt="CodeCamp Logo"></a>
     </div>
-    <img src="img/codecamp.png" alt="Logo">
     <div class="dropdown">
         <button class="dropbtn">Songs</button>
         <div class="dropdown-content">
