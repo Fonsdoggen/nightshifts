79a4fe52fdba8315054008ff2460cf8dcf0819a5
WoodierCupd <74969069+WoodierCupd@users.noreply.github.com>
2021-10-14 10:39:34 +0200

    base_url fixed

--- backend/config.php
+++ backend/config.php
@@ -1,3 +1,3 @@
 <?php
 //De url waarop jouw project staat. Géén slash aan het einde.
-$base_url = 'C:/xampp/htdocs/nightshifts';
+$base_url = 'http://localhost/nightshifts/';
--- views/header.php
+++ views/header.php
@@ -1,6 +1,7 @@
+<?php require_once 'backend/config.php'; ?>
 <header onload=display_ct();>
     <div class="logo">
-        <a href="https://agar.io"><img src="img/codecamp.png" alt="CodeCamp Logo"></a>
+        <a href="<?php echo $base_url; ?>"><img src="img/codecamp.png" alt="CodeCamp Logo"></a>
     </div>
     <div class="dropdown">
         <button class="dropbtn">Songs</button>
