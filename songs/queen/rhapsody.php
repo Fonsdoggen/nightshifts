63017e3715894dfa729057093d547f2d97a832c4
WoodierCupd <74969069+WoodierCupd@users.noreply.github.com>
2021-10-21 12:10:42 +0200

    Bohemian Rhapsody page styling

--- songs/queen/rhapsody.php
+++ songs/queen/rhapsody.php
@@ -7,69 +7,74 @@
     <link rel="stylesheet" href="../../css/main.css">
     <title>Bohemian Rhapsody</title>
 </head>
-<body class="qeeun" onload=display_ct();>
-    <?php require_once'../../views/header.php' ?>
-    <div class="song-texts">
+<body style="overflow: hidden" onload=display_ct();>
+    <div class="song-texts qeeun">
+        <?php require_once'../../views/header.php' ?>
         <h2>Bohemian Rhapsody</h2>
-        <p>
-            Is this the real life?<br>
-            Is this just fantasy?<br>
-            Caught in a landside,<br>
-            No escape from reality<br>
-            Open your eyes,<br>
-            Look up to the skies and see,<br>
-            I'm just a poor boy, I need no sympathy,<br>
-            Because I'm easy come, easy go,<br>
-            Little high, little low,<br>
-            Any way the wind blows doesn't really matter to<br>
-            Me, to me<br>
-            Mamaaa,<br>
-            Just killed a man,<br>
-            Put a gun against his head, pulled my trigger,<br>
-            Now he's dead<br>
-            Mamaaa, life had just begun,<br>
-            But now I've gone and thrown it all away<br>
-            Mama, oooh,<br>
-            Didn't mean to make you cry,<br>
-            If I'm not back again this time tomorrow,<br>
-            Carry on, carry on as if nothing really matters<br>
-            Too late, my time has come,<br>
-            Sends shivers down my spine, body's aching all<br>
-            The time<br>
-            Goodbye, everybody, I've got to go,<br>
-            Gotta leave you all behind and face the truth<br>
-            Mama, oooh<br>
-            I don't want to die,<br>
-            I sometimes wish I'd never been born at all.<br>
-            I see a little silhouetto of a man,<br>
-            Scaramouch, Scaramouch, will you do the Fandango!<br>
-            Thunderbolts and lightning, very, very frightening me<br>
-            Galileo, Galileo<br>
-            Galileo, Galileo<br>
-            Galileo, Figaro - magnificoo<br>
-            I'm just a poor boy nobody loves me<br>
-            He's just a poor boy from a poor family,<br>
-            Spare him his life from this monstrosity<br>
-            Easy come, easy go, will you let me go<br>
-            Bismillah! No, we will not let you go<br>
-            (Let him go!) Bismillah! We will not let you go<br>
-            (Let him go!) Bismillah! We will not let you go<br>
-            (Let me go) Will not let you go<br>
-            (Let me go)(Never) Never let you go<br>
-            (Let me go) (Never) let you go (Let me go) Ah<br>
-            No, no, no, no, no, no, no<br>
-            Oh mama mia, mama mia, mama mia, let me go<br>
-            Beelzebub has a devil put aside for me, for me,<br>
-            For meee<br>
-            So you think you can stop me and spit in my eye<br>
-            So you think you can love me and leave me to die<br>
-            Oh, baby, can't do this to me, baby,<br>
-            Just gotta get out, just gotta get right outta here<br>
-            Nothing really matters, Anyone can see,<br>
-            Nothing really matters,<br>
-            Nothing really matters to me<br>
-            Any way the wind blows...<br>
-        </p>
+        <div class="songContent">
+            <iframe src="https://www.youtube.com/embed/fJ9rUzIMcZQ?controls=0">
+            </iframe>
+            <p>
+                Is this the real life?<br>
+                Is this just fantasy?<br>
+                Caught in a landside,<br>
+                No escape from reality<br>
+                Open your eyes,<br>
+                Look up to the skies and see,<br>
+                I'm just a poor boy, I need no sympathy,<br>
+                Because I'm easy come, easy go,<br>
+                Little high, little low,<br>
+                Any way the wind blows doesn't really matter to<br>
+                Me, to me<br>
+                Mamaaa,<br>
+                Just killed a man,<br>
+                Put a gun against his head, pulled my trigger,<br>
+                Now he's dead<br>
+                Mamaaa, life had just begun,<br>
+                But now I've gone and thrown it all away<br>
+                Mama, oooh,<br>
+                Didn't mean to make you cry,<br>
+                If I'm not back again this time tomorrow,<br>
+                Carry on, carry on as if nothing really matters<br>
+                Too late, my time has come,<br>
+                Sends shivers down my spine, body's aching all<br>
+                The time<br>
+                Goodbye, everybody, I've got to go,<br>
+                Gotta leave you all behind and face the truth<br>
+                Mama, oooh<br>
+                I don't want to die,<br>
+                I sometimes wish I'd never been born at all.<br>
+                I see a little silhouetto of a man,<br>
+                Scaramouch, Scaramouch, will you do the Fandango!<br>
+                Thunderbolts and lightning, very, very frightening me<br>
+                Galileo, Galileo<br>
+                Galileo, Galileo<br>
+                Galileo, Figaro - magnificoo<br>
+                I'm just a poor boy nobody loves me<br>
+                He's just a poor boy from a poor family,<br>
+                Spare him his life from this monstrosity<br>
+                Easy come, easy go, will you let me go<br>
+                Bismillah! No, we will not let you go<br>
+                (Let him go!) Bismillah! We will not let you go<br>
+                (Let him go!) Bismillah! We will not let you go<br>
+                (Let me go) Will not let you go<br>
+                (Let me go)(Never) Never let you go<br>
+                (Let me go) (Never) let you go (Let me go) Ah<br>
+                No, no, no, no, no, no, no<br>
+                Oh mama mia, mama mia, mama mia, let me go<br>
+                Beelzebub has a devil put aside for me, for me,<br>
+                For meee<br>
+                So you think you can stop me and spit in my eye<br>
+                So you think you can love me and leave me to die<br>
+                Oh, baby, can't do this to me, baby,<br>
+                Just gotta get out, just gotta get right outta here<br>
+                Nothing really matters, Anyone can see,<br>
+                Nothing really matters,<br>
+                Nothing really matters to me<br>
+                Any way the wind blows...<br>
+            </p>
+        </div>
+        <p class="descriptionKiller"><b>Bohemian Rhapsody</b> is een stuk in rapsodische vorm van de Britse muziekgroep Queen. Het verscheen op het album A Night at the Opera uit 1975. Het was een wereldwijde hit voor de groep in 1975/1976 en de heruitgave in 1991 werd ook een hit. Het wordt algemeen beschouwd als een van de succesvolste popnummers aller tijden.</p>
     </div>
     
 </body>
