e828d2018565557ab6bfa715cbd607afeca0af36
Fonsdoggen <70898032+Fonsdoggen@users.noreply.github.com>
2021-10-21 11:58:48 +0200

    boek pagina toegevoegd

---
+++ wiki/boeken/boek.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../css/main.css">
+    <title>Boeken</title>
+</head>
+<body onload =display_ct();>
+    <?php require_once'../../views/header.php'?>
+    <div class="boek">
+        <h2>Boeken</h2>
+        <p>
+        </p>
+    </div>
+    <?php require_once'../../views/footer.php'?>
+</body>
+</html>
