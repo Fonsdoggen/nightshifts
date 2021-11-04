0a4df364e57ee7736b37f2821cf8bb3f99d2f0a4
Fonsdoggen <70898032+Fonsdoggen@users.noreply.github.com>
2021-11-04 11:49:32 +0100

    links toegevoegd
    links toegeveoged en de css gelinked met de personages pagina

--- personages/personages.php
+++ personages/personages.php
@@ -5,11 +5,18 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="../css/main.css">
     <title>Personages</title>
 </head>
-<body>
+<body onload=display_ct();>
     <?php require_once '../views/header.php' ?>
+    <div class="grid-container">
+        <div class="linkBox">
+            <h2>Star Wars</h2>
+            <a href="starwars/darth.php">Darth Vader</a> |
+            <a href="starwars/luke.php">Luke Skywalker</a>
+        </div>
+    </div>
     <?php require_once '../views/footer.php' ?>
 </body>
 </html>
