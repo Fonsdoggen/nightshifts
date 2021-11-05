5dfaa28fd9c602ff78173cd92b37543f6aa35383
Bramskii <bram@segboer.nl>
2021-11-05 13:07:08 +0100

    uprising rood en groen

--- songs/MyBaby/uprising.php
+++ songs/MyBaby/uprising.php
@@ -14,42 +14,44 @@
         <div class="songContent">
             <iframe src="https://www.youtube.com/embed/LkXLLpT2OSY?controls=0">
             </iframe>
-            <p>
-                Allow me in your tub<br>
-                Just allow me in your club<br>
-                Allow me to drink from your cup<br>
-                I'll be coming back for more<br>
-                Allow me in your dream<br>
-                Allow me in your scene
-                Just allow me on your knees<br>
-                I'll be coming back for more<br>
-                Sisters, keep on rising<br>
-                Brothers, keep on rising<br>
-                This girl keeps on rising<br>
-                Keep on rising up<br>
-                All I Believe in your school<br>
-                I Believe in your rules<br>
-                I Believe in your truth<br>
-                I'll be coming back for more<br>
-                I Believe in your land<br>
-                I Believe in that man<br>
-                I Believe in your command<br>
-                I'll be coming back for more<br>
-                Sisters, Brothers, Lovers<br>
-                Keep on rising up<br>
-                Sisters, keep on rising<br>
-                Brothers, keep on rising<br>
-                This girl keeps on rising<br>
-                Keep on rising up<br>
-                Uprising, uprising<br>
-                Uprising, uprising<br>
-                Sisters, Brothers, Lovers<br>
-                This girl keep on rising up<br>
-                Sisters, keep on rising<br>
-                Brothers, keep on rising<br>
-                This girl keeps on rising<br>
-                Keep on rising up<br>
-            </p>
+            <div class="uprisingtext"
+                <p>
+                    Allow me in your tub<br>
+                    Just allow me in your club<br>
+                    Allow me to drink from your cup<br>
+                    I'll be coming back for more<br>
+                    Allow me in your dream<br>
+                    Allow me in your scene
+                    Just allow me on your knees<br>
+                    I'll be coming back for more<br>
+                    Sisters, keep on rising<br>
+                    Brothers, keep on rising<br>
+                    This girl keeps on rising<br>
+                    Keep on rising up<br>
+                    All I Believe in your school<br>
+                    I Believe in your rules<br>
+                    I Believe in your truth<br>
+                    I'll be coming back for more<br>
+                    I Believe in your land<br>
+                    I Believe in that man<br>
+                    I Believe in your command<br>
+                    I'll be coming back for more<br>
+                    Sisters, Brothers, Lovers<br>
+                    Keep on rising up<br>
+                    Sisters, keep on rising<br>
+                    Brothers, keep on rising<br>
+                    This girl keeps on rising<br>
+                    Keep on rising up<br>
+                    Uprising, uprising<br>
+                    Uprising, uprising<br>
+                    Sisters, Brothers, Lovers<br>
+                    This girl keep on rising up<br>
+                    Sisters, keep on rising<br>
+                    Brothers, keep on rising<br>
+                    This girl keeps on rising<br>
+                    Keep on rising up<br>
+                </p>
+            </div>
         </div>
         <p class="descriptionKiller"><b>My Baby</b> is een Nederlandse band, opgericht in Amsterdam. My Baby ontstond in 2012 uit drie leden van de Amsterdamse soulband The Souldiers, te weten zangeres-gitariste Cato van Dijck (Marken, 1988), haar broer Joost van Dijck (Marken, 1983) op drums en gitarist Daniel de Vries (Amsterdam, 1981).</p>
     </div>
