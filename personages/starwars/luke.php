87760a567b407da16dd85a8a9a14000b4f2d4047
Fonsdoggen <70898032+Fonsdoggen@users.noreply.github.com>
2021-11-05 12:08:33 +0100

    luke en darth vader
    luke skywlker pagina toegevoegd en een foto van hem toegeoegd

--- personages/starwars/luke.php
+++ personages/starwars/luke.php
@@ -8,7 +8,10 @@
     <title>Luke Skywalker</title>
 </head>
 <body onload=display_ct();>
-    <?php require_once '../views/header.php' ?>
-    <?php require_once '../views/footer.php' ?>
+    <?php require_once '../../views/header.php' ?>
+    <h2>Luke Skywalker</h2>
+    <p> Luke speelt een grote rol in het Star Wars Expanded Universe. Hierin is het leven van Luke Skywalker zeer uitgebreid (ook over de periodes door de films worden overlapt, maar niet worden getoond). Het is bijvoorbeeld Luke die de locatie op Hoth vindt, waar de rebellen zich hebben gehergroepeerd in Episode V. Tevens wordt hij tussentijds verliefd op Shira Brie, die hij kort daarna per ongeluk zou doden, tijdens een gevecht waarbij hij wederom moest vertrouwen op zijn kracht als Jedi. Maar later zou blijken dat Brie een geheim agente van het Keizerrijk was. Na de slag op Endor gaat zijn verhaal verder. Zo zou Luke na de films een zeer gerespecteerd Jedi-meester worden en vele generaties Padawan opleiden om de eeuwenoude traditie van de Jedi opnieuw te doen opleven. Tevens ontmoet hij Mara Jade, die in eerste instantie erop uit is om hem te doden, maar later krijgen ze gevoelens voor elkaar en trouwen ze.</p>
+    <img src="../../img/luke.jpg" alt="Foto Luke Skywalker" class="resize">
+    <?php require_once '../../views/footer.php' ?>
 </body>
 </html>
