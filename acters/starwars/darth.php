59d9830ace8bf325083ab52028a7ff0a661d2966
Thomasbro312 <73693551+Thomasbro312@users.noreply.github.com>
2021-11-08 14:08:36 +0100

    Witdh ajusted

--- acters/starwars/darth.php
+++ acters/starwars/darth.php
@@ -10,9 +10,11 @@
 <body onload=display_ct(); class="yellow">
     <?php require_once '../../views/header.php' ?>
     <div class="center">
+        <div class="article">
         <h2>Darth Vader</h2>
             <p>In de eerste drie opgenomen films (Episodes IV, V en VI) speelt David Prowse de rol van Darth Vader (behalve in enkele gevechtsscènes, waar Bob Anderson hem speelt). De stem wordt gedubd door de acteur James Earl Jones. In deze films is het gezicht van Darth Vader slechts twee keer te zien. Vlak voordat Darth Vader overlijdt, in Episode VI, wordt zijn masker afgedaan en wordt hij gespeeld door Sebastian Shaw. Later is hij te zien als geest. In de oorspronkelijke versie is Shaw hier opnieuw te zien, maar in de opnieuw uitgebrachte dvd-versie is deze geestverschijning aangepast, zodat Hayden Christensen te zien is. Hayden speelde Anakin Skywalker in de prequeltrilogie (Episodes I, II en III), het personage wat aan het eind van Episode III transformeert in Darth Vader.Daarnaast wordt Darth Vader ook nog door andere acteurs gespeeld in een aantal videospellen. In een hoorspel uit de jaren 80 en 90 werd zijn stem door Brock Peters ingesproken. Voor verschillende videospellen werd zijn stem ingesproken door Matt Sloan en later door Scott Lawrence.</p>
             <img src="../../img/darth.png" alt="Foto Darth Vader" class="resize">
+        </div>
     </div>
     <?php require_once '../../views/footer.php' ?>
 </body>
