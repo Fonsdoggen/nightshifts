9cd277329cf326fc2112a2c537534bc233ea5c15
Thomasbro312 <73693551+Thomasbro312@users.noreply.github.com>
2021-11-08 13:12:43 +0100

    Padding en iframe toegevoegd

--- index.php
+++ index.php
@@ -13,6 +13,7 @@
     <main>
         <div class="bigLogo">
             <img src="img/CodeCampBig.png" alt="Background">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1569.7345058471974!2d5.29279082887492!3d51.697623762662694!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c6ee66bc48c005%3A0x4dddc528356e3d4f!2sKoestraat%201%2C%205223%20AG%20&#39;s-Hertogenbosch!5e1!3m2!1sen!2snl!4v1636372939711!5m2!1sen!2snl" width="300" height="350" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
         </div>
     </main>
     
