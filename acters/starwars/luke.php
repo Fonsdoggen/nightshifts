a15c5bdc8d2d77d2de2bc16451ad4ebe55bbb22c
Bramskii <bram@segboer.nl>
2021-11-11 11:58:47 +0100

    skywalker

--- acters/starwars/luke.php
+++ acters/starwars/luke.php
@@ -8,15 +8,21 @@
     <title>Luke Skywalker</title>
 </head>
 <body onload=display_ct(); class="green">
-    <div class="center">
+    <?php require_once '../../views/header.php' ?>
+    <div class="skywalker">
+        <div class="skywalkertext">
+            <h2>Luke Skywalker</h2>
+            <p>Luke speelt een grote rol in het Star Wars Expanded Universe. Hierin is het leven van Luke Skywalker zeer uitgebreid (ook over de periodes door de films worden overlapt, maar niet worden getoond). Het is bijvoorbeeld Luke die de locatie op Hoth vindt, waar de rebellen zich hebben gehergroepeerd in Episode V. Tevens wordt hij tussentijds verliefd op Shira Brie, die hij kort daarna per ongeluk zou doden, tijdens een gevecht waarbij hij wederom moest vertrouwen op zijn kracht als Jedi. </p>
+            <p>Maar later zou blijken dat Brie een geheim agente van het Keizerrijk was. Na de slag op Endor gaat zijn verhaal verder. Zo zou Luke na de films een zeer gerespecteerd Jedi-meester worden en vele generaties Padawan opleiden om de eeuwenoude traditie van de Jedi opnieuw te doen opleven. Tevens ontmoet hij Mara Jade, die in eerste instantie erop uit is om hem te doden, maar later krijgen ze gevoelens voor elkaar en trouwen ze.</p>
+        </div>
 
-        <?php require_once '../../views/header.php' ?>
-        <div class="article">
-        <h2>Luke Skywalker</h2>
-        <p> Luke speelt een grote rol in het Star Wars Expanded Universe. Hierin is het leven van Luke Skywalker zeer uitgebreid (ook over de periodes door de films worden overlapt, maar niet worden getoond). Het is bijvoorbeeld Luke die de locatie op Hoth vindt, waar de rebellen zich hebben gehergroepeerd in Episode V. Tevens wordt hij tussentijds verliefd op Shira Brie, die hij kort daarna per ongeluk zou doden, tijdens een gevecht waarbij hij wederom moest vertrouwen op zijn kracht als Jedi. Maar later zou blijken dat Brie een geheim agente van het Keizerrijk was. Na de slag op Endor gaat zijn verhaal verder. Zo zou Luke na de films een zeer gerespecteerd Jedi-meester worden en vele generaties Padawan opleiden om de eeuwenoude traditie van de Jedi opnieuw te doen opleven. Tevens ontmoet hij Mara Jade, die in eerste instantie erop uit is om hem te doden, maar later krijgen ze gevoelens voor elkaar en trouwen ze.</p>
-        <img src="../../img/luke.jpg" alt="Foto Luke Skywalker" class="resize">
-            </div>
+        <div class="skywalkerimg">
+            <img src="../../img/skywalker.png" alt="Luke Skywalker">
+        </div>
+    </div>
+
+
         <?php require_once '../../views/footer.php' ?>
-    </body>
-</class>
+</body>
+
 </html>
