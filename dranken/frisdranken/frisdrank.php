eae3841d8662a3ba384fa898342a184ec1a10cdf
Fonsdoggen <70898032+Fonsdoggen@users.noreply.github.com>
2021-11-11 12:16:22 +0100

    container

--- dranken/frisdranken/frisdrank.php
+++ dranken/frisdranken/frisdrank.php
@@ -9,13 +9,15 @@
 </head>
 <body  onload=display_ct(); class="orange">
     <?php require_once'../../views/header.php' ?>
-    <h2>Coca Cola</h2>
-    <p>Coca-Cola is een merk cola. Het is een suikerhoudende koolzuurhoudende frisdrank die wordt verhandeld door The Coca-Cola Company. Het is sinds 1893 als handelsmerk geregistreerd. Inmiddels zijn het logo, de flessen en de drank samen een wereldwijd bekend icoon voor de Amerikaanse levensstijl.</p>
-    <a href="<?php echo $base_url; ?>acters/007/moore.php">Roger Moore</a>
-    <h2>Up</h2>
-    <p>7Up (of Seven-Up) is een frisdrankmerk van Dr Pepper Snapple Group in de Verenigde Staten en PepsiCo in de rest van de wereld.</p>
-    <h2>Sinas</h2>
-    <p>Sinas is in Nederland de benaming voor de doorgaans gele of oranje, meestal koolzuurhoudende frisdrank met sinaasappelsmaak. Zoals de meeste frisdranken bevat sinas veel suiker of in de light-versie kunstmatige zoetstoffen. Het gehalte aan sinaasappelsap bedraagt ten minste 4%. Bekende merken zijn Fanta (van de Coca-Cola Company) en Sisi (Vrumona); Orangina onderscheidt zich doordat er werkelijk vruchtvlees in zit.</p>
-<!--    <a href="../drank.php"><button>Terug</button></a>-->
+    <div class="container">
+        <h2>Coca Cola</h2>
+        <p>Coca-Cola is een merk cola. Het is een suikerhoudende koolzuurhoudende frisdrank die wordt verhandeld door The Coca-Cola Company. Het is sinds 1893 als handelsmerk geregistreerd. Inmiddels zijn het logo, de flessen en de drank samen een wereldwijd bekend icoon voor de Amerikaanse levensstijl.</p>
+        <a href="<?php echo $base_url; ?>acters/007/moore.php">Roger Moore</a>
+        <h2>Up</h2>
+        <p>7Up (of Seven-Up) is een frisdrankmerk van Dr Pepper Snapple Group in de Verenigde Staten en PepsiCo in de rest van de wereld.</p>
+        <h2>Sinas</h2>
+        <p>Sinas is in Nederland de benaming voor de doorgaans gele of oranje, meestal koolzuurhoudende frisdrank met sinaasappelsmaak. Zoals de meeste frisdranken bevat sinas veel suiker of in de light-versie kunstmatige zoetstoffen. Het gehalte aan sinaasappelsap bedraagt ten minste 4%. Bekende merken zijn Fanta (van de Coca-Cola Company) en Sisi (Vrumona); Orangina onderscheidt zich doordat er werkelijk vruchtvlees in zit.</p>
+        <!--    <a href="../drank.php"><button>Terug</button></a>-->
+    </div>
 </body>
 </html>
--- wiki/tools/schroevendraaier.php
+++ wiki/tools/schroevendraaier.php
@@ -9,13 +9,12 @@
 </head>
 <body onload=display_ct();>
 <?php require_once'../../views/header.php' ?>
-<div class="article">
+<div class="container">
     <h1>Schroevendraaier</h1>
-<p>Een schroevendraaier is een stuk gereedschap waarmee schroeven worden aangebracht en verwijderd. Het bestaat uit een handvat met een metalen steel waarvan het uiteinde zo gevormd is dat het in de kop van een schroef past.
-
-    Schroevendraaiers zijn in velerlei soorten, afmetingen en kwaliteiten verkrijgbaar. Er bestaan schroevendraaiers met een platte punt, alsook met een ster- of kruiskop. Goede schroevendraaiers zijn vervaardigd van speciale staalsoorten. Bij een juiste toepassing zijn ze praktisch onbreekbaar. Het heft van een schroevendraaier wordt meestal gemaakt van een slagvaste, onbreekbare kunststof. Soms vervaardigt men het heft nog van een taaie houtsoort. De grootte van schroevendraaier en schroef dienen bij elkaar te 'passen'.</p>
+    <p>Een schroevendraaier is een stuk gereedschap waarmee schroeven worden aangebracht en verwijderd. Het bestaat uit een handvat met een metalen steel waarvan het uiteinde zo gevormd is dat het in de kop van een schroef past.
+        Schroevendraaiers zijn in velerlei soorten, afmetingen en kwaliteiten verkrijgbaar. Er bestaan schroevendraaiers met een platte punt, alsook met een ster- of kruiskop. Goede schroevendraaiers zijn vervaardigd van speciale staalsoorten. Bij een juiste toepassing zijn ze praktisch onbreekbaar. Het heft van een schroevendraaier wordt meestal gemaakt van een slagvaste, onbreekbare kunststof. Soms vervaardigt men het heft nog van een taaie houtsoort. De grootte van schroevendraaier en schroef dienen bij elkaar te 'passen'.</p>
+    <img src="../../img/schroevendraaier.jpg" alt="schroevendraaier" class="imgtools">
 </div>
-<div class="imgtools"><img src="../../img/schroevendraaier.jpg" alt="schroevendraaier"></div>
 <?php require_once'../../views/footer.php' ?>
 </body>
 </html>
