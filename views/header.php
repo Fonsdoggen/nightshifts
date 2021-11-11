f2d7f4648eef6059b4d838c6311e4efafb2dba07
WoodierCupd <74969069+WoodierCupd@users.noreply.github.com>
2021-11-11 12:06:41 +0100

    header "fix"

--- views/header.php
+++ views/header.php
@@ -9,7 +9,7 @@
     }
     ?>
     <div class="logo">
-        <a href="<?php echo $base_url; ?>"><img src="<?php echo $base_url; ?>img/codecamp.png" alt="CodeCamp Logo"></a>
+        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"><img src="<?php echo $base_url; ?>img/codecamp.png" alt="CodeCamp Logo"></a>
     </div>
     <div class="nav">
         <a href="<?php echo $base_url; ?>"><button class="dropbtn <?php if (strpos($_SERVER['REQUEST_URI'], ".php") == false){echo "active";} ?>">Home</button></a>
