331e907c12b0e00c55072b23f45c7226a0a21681
Fonsdoggen <70898032+Fonsdoggen@users.noreply.github.com>
2021-11-11 10:58:19 +0100

    we are the champions toegevoegd
    we are the champions pagina nog toegevoegd want die hadden we over het hoofd gezien

--- songs/queen/rhapsody.php
+++ songs/queen/rhapsody.php
@@ -8,7 +8,7 @@
     <title>Bohemian Rhapsody</title>
 </head>
 <body onload=display_ct();>
-    <div class="song-texts">
+    <div class="song-texts qeeun">
         <?php require_once'../../views/header.php' ?>
         <h2>Bohemian Rhapsody</h2>
         <div class="songContent" >
