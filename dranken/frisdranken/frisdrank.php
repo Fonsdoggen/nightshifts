ca9ac0c360915e3391dc49bcf43c63efe23d89c3
Thomasbro312 <73693551+Thomasbro312@users.noreply.github.com>
2021-11-12 10:50:14 +0100

    updated frisdrank pagina

--- dranken/frisdranken/frisdrank.php
+++ dranken/frisdranken/frisdrank.php
@@ -12,11 +12,14 @@
     <div class="container-admin">
         <h2>Coca Cola</h2>
         <p>Coca-Cola is een merk cola. Het is een suikerhoudende koolzuurhoudende frisdrank die wordt verhandeld door The Coca-Cola Company. Het is sinds 1893 als handelsmerk geregistreerd. Inmiddels zijn het logo, de flessen en de drank samen een wereldwijd bekend icoon voor de Amerikaanse levensstijl.</p>
+        <img class="imgtools"  src="../../img/cola.png" alt="Coca Cola" >
         <a href="<?php echo $base_url; ?>acters/007/moore.php">Roger Moore</a>
         <h2>Up</h2>
         <p>7Up (of Seven-Up) is een frisdrankmerk van Dr Pepper Snapple Group in de Verenigde Staten en PepsiCo in de rest van de wereld.</p>
+        <img class="imgtools" src="../../img/7-Up.png" alt="7-up">
         <h2>Sinas</h2>
         <p>Sinas is in Nederland de benaming voor de doorgaans gele of oranje, meestal koolzuurhoudende frisdrank met sinaasappelsmaak. Zoals de meeste frisdranken bevat sinas veel suiker of in de light-versie kunstmatige zoetstoffen. Het gehalte aan sinaasappelsap bedraagt ten minste 4%. Bekende merken zijn Fanta (van de Coca-Cola Company) en Sisi (Vrumona); Orangina onderscheidt zich doordat er werkelijk vruchtvlees in zit.</p>
+        <img class="imgtools" src="../../img/sinas.png" alt="Fanta" style="width: 300px">
     </div>
     <?php require_once '../../views/footer.php' ?>
 </body>
