72c3940c17b770ff324c8f247c235ff15e5b820c
Fonsdoggen <70898032+Fonsdoggen@users.noreply.github.com>
2021-11-12 11:14:10 +0100

    boeken en css
    ik heb de boeken pagina gefixed en css toegevegd bij de container

--- wiki/boeken/boek.php
+++ wiki/boeken/boek.php
@@ -9,14 +9,14 @@
 </head>
 <body onload =display_ct();>
     <?php require_once'../../views/header.php'?>
+    <div class="container">
     <h2>Boeken</h2>
-    <div class="boek">
-        <div class="boektext">
-            <p>Een boek in traditionele zin is een samengevoegde hoeveelheid papier met tekst, afbeeldingen of allebei. Daarnaast wordt met de term de betreffende informatie, of een soortgelijke verzameling informatie, aangeduid ('een boek schrijven', 'een boek downloaden').</p>
-            <p>Het woord 'boek' is vermoedelijk afkomstig van het Germaanse woord voor beuk, boche. De omslagen van de eerste boeken werden namelijk gemaakt van een rechthoekig stuk (beuken)hout. Het Griekse woord biblia (boeken) is het meervoud van biblion dat oorspronkelijk 'papyrusje' betekende, het materiaal waarop in de oudheid meestal werd geschreven, en later werd gebruikt als aanduiding van papier, geschrift en boek(rol).</p>
-        </div>
-        <div class="boekimg">
-            <img src="../../img/boeken.jpg" alt="Stapel boeken">
+        <div class="boek">
+            <div class="boektext">
+                <p>Een boek in traditionele zin is een samengevoegde hoeveelheid papier met tekst, afbeeldingen of allebei. Daarnaast wordt met de term de betreffende informatie, of een soortgelijke verzameling informatie, aangeduid ('een boek schrijven', 'een boek downloaden').</p>
+                <p>Het woord 'boek' is vermoedelijk afkomstig van het Germaanse woord voor beuk, boche. De omslagen van de eerste boeken werden namelijk gemaakt van een rechthoekig stuk (beuken)hout. Het Griekse woord biblia (boeken) is het meervoud van biblion dat oorspronkelijk 'papyrusje' betekende, het materiaal waarop in de oudheid meestal werd geschreven, en later werd gebruikt als aanduiding van papier, geschrift en boek(rol).</p>
+            </div>
+                <img src="../../img/boeken.jpg" alt="Stapel boeken" class="resize">
         </div>
     </div>
     <?php require_once'../../views/footer.php'?>
